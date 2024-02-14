2d397ee6f07ddfb24711508d2508bbd950d5cd37
Aniruddh Balram <abalram1@umd.edu>
2024-02-14 05:35:42 -0500

    * create_node_copy: To create a copied instance of a node
    * swap_position: To swap the states of two nodes

--- helper/helper.py
+++ helper/helper.py
@@ -17,3 +17,17 @@ class BFS:
         initial_node = PuzzleNode(self.initial_state, 0, None)
         self.visited_nodes.append(initial_node)
         self.node_queue.append(initial_node)
+        
+    def create_node_copy(self, node):
+        copied_node = PuzzleNode(None, None, None)
+        copied_node.node_index_i = node.node_index_i
+        copied_node.parent_node_index_i = node.parent_node_index_i
+        copied_node.node_state_i = node.node_state_i.copy()
+        return copied_node
+
+    def swap_position(self, node, curr_y, curr_x, new_y, new_x):
+        curr_val = node.node_state_i[curr_y][curr_x]
+        new_val = node.node_state_i[new_y][new_x]
+        node.node_state_i[curr_y][curr_x] = new_val
+        node.node_state_i[new_y][new_x] = curr_val
+        return node
