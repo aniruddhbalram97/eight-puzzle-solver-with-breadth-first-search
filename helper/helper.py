3caa44db707540217381c57f53364bb73b9c4405
Aniruddh Balram <abalram1@umd.edu>
2024-02-14 05:33:29 -0500

    * Class definition for BFS with initial_state and goal_state as inputs

--- helper/helper.py
+++ helper/helper.py
@@ -6,3 +6,14 @@ class PuzzleNode:
         self.node_index_i = node_index_i
         self.parent_node_index_i = parent_node_index_i
 
+class BFS:
+    def __init__(self, initial_state, goal_state):
+        self.initial_state = initial_state
+        self.goal_state = goal_state
+        self.visited_nodes = []
+        self.node_queue = []
+        self.path_taken = []
+        
+        initial_node = PuzzleNode(self.initial_state, 0, None)
+        self.visited_nodes.append(initial_node)
+        self.node_queue.append(initial_node)
