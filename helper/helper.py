96e217528e6dd1a22b6529aabf8194cfa9aa44e7
Aniruddh Balram <abalram1@umd.edu>
2024-02-14 05:31:47 -0500

    * Add class for puzzle_node defining it's state, index and parent_index

---
+++ helper/helper.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class PuzzleNode:
+    def __init__(self, node_state_i, node_index_i, parent_node_index_i):
+        self.node_state_i = node_state_i
+        self.node_index_i = node_index_i
+        self.parent_node_index_i = parent_node_index_i
+
